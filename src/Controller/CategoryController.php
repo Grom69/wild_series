faa13e410a846162de435cb6ee10f4799a3fd9bb
Grom69 <jerome.dannfald@orange.fr>
2022-12-01 12:15:03 +0100

    quest 10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[route('/category', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', ['categories' => $categories]);
+    }
+
+    #[route('/{categoryName}', name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository)
+    {
+        $category = $categoryRepository->findOneBy(
+            ['name' => $categoryName]
+        );
+
+        if (!$category)
+            throw $this->createNotFoundException();
+
+        $programs = $programRepository->findBy(
+            ['category' => $category],
+            ['id' => 'DESC'],
+            3,
+            0
+        );
+
+        return $this->render('category/show.html.twig', ['programs' => $programs, 'categoryName' => $categoryName]);
+    }
+}
